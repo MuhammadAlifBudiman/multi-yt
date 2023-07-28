162429506ddce5ca66444038490a755f804ffecf
coleaderme <coleaderme@gmail.com>
2023-07-28 14:52:02 +0530

    (*_-)

--- yt_pl_dl.py
+++ yt_pl_dl.py
@@ -24,23 +24,17 @@ groups = int(total / BATCH_SIZE)
 extra = total % BATCH_SIZE
 
 b = 0
-## loop 11 times of 5's
 for j in range(groups):
-    ## links[0:0+5] >> links[5:5+5] >> links[10:10+5]
     with open(f"queue{j+1}.txt", 'a') as f:
         for k in links[b:b+BATCH_SIZE]:
             f.write(f"{k}\n")
-            # print(k)
         b = b + BATCH_SIZE
-        # print("\n\n")
 
 if extra:
     with open(f"queue{j+1+1}.txt", 'a') as f:
         for r in links[b:]:
             f.write(f"{r}\n")
-            # print("extra "+ r)
 
 print("total items:", total,"groups:", groups ,"extra:", extra)
-
-## Step 3: Execute this command
+## Step 3: Execute yt-dlp (*-*)+--
 !for i in $(ls | grep "queue"); do bash multiYT.sh $i;done
